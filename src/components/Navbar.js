cd5073660a6015189d0f790ba144c912176bf425
Emma Mansell <emilyjmansell@gmail.com>
2023-02-27 23:10:31 -0600

    z

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <nav className="sticky top-0 z-10 bg-[#5cdb95] backdrop-filter backdrop-blur-lg bg-opacity-30">
+        <nav className="sticky top-0 z-20 bg-[#5cdb95] backdrop-filter backdrop-blur-lg bg-opacity-30">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex items-center justify-between h-16">
                     <span className="text-2xl text-[#05386b] font-semibold">Emma Mansell</span>
