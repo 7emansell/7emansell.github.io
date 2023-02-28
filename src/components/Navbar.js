03a0926d5fa8da10b47350d12c543db30a8eeb3b
Emma Mansell <emilyjmansell@gmail.com>
2023-02-27 23:20:23 -0600

    star

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
         <nav className="sticky top-0 z-20 bg-[#5cdb95] backdrop-filter backdrop-blur-lg bg-opacity-30">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex items-center justify-between h-16">
-                    <span className="text-2xl text-[#05386b] font-semibold">Emma Mansell</span>
+                    <span className="text-2xl text-[#05386b] font-semibold"> <a href="#">Emma Mansell </a></span>
                     <div className="flex space-x-4 text-[#05386b]">
                         <a href="#about">About</a>
                         <a href="#projects">Projects</a>
